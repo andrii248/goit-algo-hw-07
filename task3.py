4d8ecc610d31c7bc6e27ae0c9dd22049f6431fb0
Andrii Krupa <krupaandrii@gmail.com>
2025-01-28 20:04:45 +0100

    hw6 submit

---
+++ task3.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\t" * level + prefix + str(self.val) + "\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+
+def sum_of_tree(node):
+    if node is None:
+        return 0
+    return node.val + sum_of_tree(node.left) + sum_of_tree(node.right)
+
+
+# Test
+root = Node(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+
+# Знаходимо суму всіх значень
+tree_sum = sum_of_tree(root)
+print("Сума всіх значень у дереві:", tree_sum)
